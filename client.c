ec39891b18619f88acecc991646da446de65e8bb
jeansanson <jeancsanson@gmail.com>
2020-05-29 00:08:31 +0000

    Conecta ao servidor da google

--- client.c
+++ client.c
@@ -1,14 +1,26 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
 
-    /*AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP*/
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+  int socket_desc;
+  struct sockaddr_in server;
 
-    if(socket_desc == -1){
-        printf("Não foi possível criar o sockt");
-        return 1;
-    }
-    return 0;
+  // AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP
+  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+  server.sin_addr.s_addr = inet_addr("172.217.28.132");
+  server.sin_family = AF_INET;
+  server.sin_port = htons(80);
+
+  printf("opening connection...\n");
+
+  // connect
+  if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    printf("error\n");
+    return 1;
+  }
+  printf("sucess\n");
+
+  return 0;
 }
